5aa568992b7dbceda5d1f4c4cbc0ba39768b85b4
Jan Buchar <Teyras@gmail.com>
2023-01-24 23:21:21 +0100

    Use JsonSchema for list responses

--- src/openapiSpec.ts
+++ src/openapiSpec.ts
@@ -31,174 +31,7 @@ export const generateV30Spec = async (
         url: `${req.secure ? 'https' : 'http'}://${req.header('host')}`,
       },
     ],
-    paths: Object.fromEntries(
-      Object.entries(req.payload.collections).flatMap(([slug, collection]) => {
-        const singular = getTranslation(collection.config.labels.singular, req.i18n)
-        const plural = getTranslation(collection.config.labels.plural, req.i18n)
-        const tags = [plural]
-
-        const singleObjectResponses = {
-          200: {
-            description: `${singular} object`,
-            content: {
-              'text/json': {
-                schema: { $ref: `#/components/schemas/${slug}` },
-              },
-            },
-          },
-          404: {
-            description: `${singular} not found`,
-          },
-        } satisfies OpenAPIV3.ResponsesObject
-
-        return [
-          [
-            `/api/${slug}`,
-            {
-              get: {
-                summary: `Retrieve a list of ${plural}`,
-                tags,
-                parameters: [
-                  { in: 'query', name: 'page', schema: { type: 'number' } },
-                  { in: 'query', name: 'limit', schema: { type: 'number' } },
-                  {
-                    in: 'query',
-                    name: 'sort',
-                    schema: {
-                      type: 'string',
-                      enum: collection.config.fields.flatMap(field => {
-                        if (
-                          field.type === 'number' ||
-                          field.type === 'text' ||
-                          field.type === 'email' ||
-                          field.type === 'date'
-                        ) {
-                          return [field.name, `-${field.name}`]
-                        }
-                        return []
-                      }),
-                    },
-                  },
-                  {
-                    in: 'query',
-                    name: 'where',
-                    style: 'deepObject',
-                    schema: { type: 'object', additionalProperties: true }, // TODO a more thorough description
-                  },
-                ],
-                responses: {
-                  200: {
-                    description: `List of ${plural}`,
-                    content: {
-                      'text/json': {
-                        schema: {
-                          // TODO use jsonschema and convert
-                          type: 'object',
-                          properties: {
-                            docs: {
-                              type: 'array',
-                              items: { $ref: `#/components/schemas/${slug}` },
-                            },
-                            totalDocs: { type: 'number' },
-                            limit: { type: 'number' },
-                            totalPages: { type: 'number' },
-                            page: { type: 'number' },
-                            pagingCounter: { type: 'number' },
-                            hasPrevPage: { type: 'boolean' },
-                            hasNextPage: { type: 'boolean' },
-                            prevPage: { type: 'number', nullable: true },
-                            nextPage: { type: 'number', nullable: true },
-                          },
-                          required: [
-                            'docs',
-                            'totalDocs',
-                            'limit',
-                            'totalPages',
-                            'page',
-                            'pagingCounter',
-                            'hasPrevPage',
-                            'hasNextPage',
-                            'prevPage',
-                            'nextPage',
-                          ],
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-              post: {
-                summary: `Create a new ${singular}`,
-                tags,
-                requestBody: { $ref: `#/components/requestBodies/${slug}` },
-                responses: {
-                  201: {
-                    description: `${singular} created successfully`,
-                    content: {
-                      'text/json': {
-                        schema: {
-                          type: 'object',
-                          properties: {
-                            message: { type: 'string' },
-                            doc: {
-                              type: 'object',
-                              properties: {
-                                id: { type: 'string' },
-                                createdAt: {
-                                  type: 'string',
-                                  format: 'date-time',
-                                },
-                                updatedAt: {
-                                  type: 'string',
-                                  format: 'date-time',
-                                },
-                              },
-                              required: ['id', 'createdAt', 'updatedAt'],
-                            },
-                          },
-                          required: ['message', 'doc'],
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            } satisfies OpenAPIV3.PathItemObject,
-          ],
-          [
-            `/api/${slug}/{id}`,
-            {
-              parameters: [
-                {
-                  in: 'path',
-                  name: 'id',
-                  description: `ID of the ${singular}`,
-                  required: true,
-                  schema: {
-                    type: 'string',
-                  },
-                },
-              ],
-              get: {
-                summary: `Find a ${singular} by ID`,
-                tags,
-                responses: singleObjectResponses,
-              },
-              patch: {
-                summary: `Update a ${singular}`,
-                tags,
-                responses: singleObjectResponses,
-              },
-              delete: {
-                summary: `Delete a ${singular}`,
-                tags,
-                responses: singleObjectResponses,
-              },
-            } satisfies OpenAPIV3.PathItemObject,
-          ],
-        ]
-      }),
-    ),
+    paths: {} as OpenAPIV3.PathsObject,
     components: {
       schemas: Object.fromEntries(
         await Promise.all(
@@ -247,6 +80,165 @@ export const generateV30Spec = async (
       ),
     },
   } satisfies OpenAPIV3.Document
+
+  for (const [slug, collection] of Object.entries(req.payload.collections)) {
+    const singular = getTranslation(collection.config.labels.singular, req.i18n)
+    const plural = getTranslation(collection.config.labels.plural, req.i18n)
+    const tags = [plural]
+
+    const singleObjectResponses = {
+      200: {
+        description: `${singular} object`,
+        content: {
+          'text/json': {
+            schema: { $ref: `#/components/schemas/${slug}` },
+          },
+        },
+      },
+      404: {
+        description: `${singular} not found`,
+      },
+    } satisfies OpenAPIV3.ResponsesObject
+
+    spec.paths[`/api/${slug}`] = {
+      get: {
+        summary: `Retrieve a list of ${plural}`,
+        tags,
+        parameters: [
+          { in: 'query', name: 'page', schema: { type: 'number' } },
+          { in: 'query', name: 'limit', schema: { type: 'number' } },
+          {
+            in: 'query',
+            name: 'sort',
+            schema: {
+              type: 'string',
+              enum: collection.config.fields.flatMap(field => {
+                if (
+                  field.type === 'number' ||
+                  field.type === 'text' ||
+                  field.type === 'email' ||
+                  field.type === 'date'
+                ) {
+                  return [field.name, `-${field.name}`]
+                }
+                return []
+              }),
+            },
+          },
+          {
+            in: 'query',
+            name: 'where',
+            style: 'deepObject',
+            schema: { type: 'object', additionalProperties: true }, // TODO a more thorough description
+          },
+        ],
+        responses: {
+          200: {
+            description: `List of ${plural}`,
+            content: {
+              'text/json': {
+                schema: await jsonSchemaToOpenapiSchema({
+                  type: 'object',
+                  properties: {
+                    docs: {
+                      type: 'array',
+                      items: { $ref: `#/components/schemas/${slug}` },
+                    },
+                    totalDocs: { type: 'number' },
+                    limit: { type: 'number' },
+                    totalPages: { type: 'number' },
+                    page: { type: 'number' },
+                    pagingCounter: { type: 'number' },
+                    hasPrevPage: { type: 'boolean' },
+                    hasNextPage: { type: 'boolean' },
+                    prevPage: { type: ['number', 'null'] },
+                    nextPage: { type: ['number', 'null'] },
+                  },
+                  required: [
+                    'docs',
+                    'totalDocs',
+                    'limit',
+                    'totalPages',
+                    'page',
+                    'pagingCounter',
+                    'hasPrevPage',
+                    'hasNextPage',
+                    'prevPage',
+                    'nextPage',
+                  ],
+                }),
+              },
+            },
+          },
+        },
+      },
+      post: {
+        summary: `Create a new ${singular}`,
+        tags,
+        requestBody: { $ref: `#/components/requestBodies/${slug}` },
+        responses: {
+          201: {
+            description: `${singular} created successfully`,
+            content: {
+              'text/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    message: { type: 'string' },
+                    doc: {
+                      type: 'object',
+                      properties: {
+                        id: { type: 'string' },
+                        createdAt: {
+                          type: 'string',
+                          format: 'date-time',
+                        },
+                        updatedAt: {
+                          type: 'string',
+                          format: 'date-time',
+                        },
+                      },
+                      required: ['id', 'createdAt', 'updatedAt'],
+                    },
+                  },
+                  required: ['message', 'doc'],
+                },
+              },
+            },
+          },
+        },
+      },
+    } satisfies OpenAPIV3.PathItemObject
+
+    spec.paths[`/api/${slug}/{id}`] = {
+      parameters: [
+        {
+          in: 'path',
+          name: 'id',
+          description: `ID of the ${singular}`,
+          required: true,
+          schema: {
+            type: 'string',
+          },
+        },
+      ],
+      get: {
+        summary: `Find a ${singular} by ID`,
+        tags,
+        responses: singleObjectResponses,
+      },
+      patch: {
+        summary: `Update a ${singular}`,
+        tags,
+        responses: singleObjectResponses,
+      },
+      delete: {
+        summary: `Delete a ${singular}`,
+        tags,
+        responses: singleObjectResponses,
+      },
+    } satisfies OpenAPIV3.PathItemObject
+  }
 
   adjustRefTargets(spec)
 
