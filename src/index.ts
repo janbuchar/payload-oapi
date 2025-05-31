27ac1e0f672d1bb32b6796c4ee7ea5bf07cc7e24
Santosh Bhattarai <20886273+sbbullet@users.noreply.github.com>
2025-05-31 17:26:44 +0545

    fix: add missing Scalar plugin import to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import openapi from './openapiPlugin.js'
 import rapidoc from './rapidocPlugin.js'
 import redoc from './redocPlugin.js'
 import swaggerUI from './swaggerUIPlugin.js'
+import scalar from './scalarPlugin.js'
 
-export { openapi, swaggerUI, rapidoc, redoc }
+export { openapi, swaggerUI, rapidoc, redoc, scalar };
