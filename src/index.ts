b7cc8ce12a2227af30b00c6f7088a4ebbc15b9fb
Santosh Bhattarai <20886273+sbbullet@users.noreply.github.com>
2025-05-31 17:32:41 +0545

    fix: resolve linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import openapi from './openapiPlugin.js'
 import rapidoc from './rapidocPlugin.js'
 import redoc from './redocPlugin.js'
-import swaggerUI from './swaggerUIPlugin.js'
 import scalar from './scalarPlugin.js'
+import swaggerUI from './swaggerUIPlugin.js'
 
-export { openapi, swaggerUI, rapidoc, redoc, scalar };
+export { openapi, swaggerUI, rapidoc, redoc, scalar }
